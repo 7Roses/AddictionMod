11f41d0eb0079245f681f1b2dd625f9bf37d32fd
7Roses <rijckdejoris@hotmail.com>
2013-08-06 18:10:44 +0200

    forgot to select some files, see priv note

---
+++ media/lua/mods/AddictionMod/README.txt
@@ -0,0 +1,7 @@
+################################
+# README FILE FOR ADDICTIONMOD #
+################################
+
+
+this mod add's some drug's to the PZ mix
+also some other addictive items will be added by this mod.
---
+++ media/lua/mods/AddictionMod/TimedActions/TakePillAction.lua
@@ -0,0 +1,49 @@
+--***********************************************************
+--**                    ROBERT JOHNSON                     **
+--***********************************************************
+
+require "TimedActions/ISBaseTimedAction"
+
+TakePillAction = ISBaseTimedAction:derive("TakePillAction");
+
+function TakePillAction:isValid()
+	return self.character:getInventory():contains(self.item);
+end
+
+function TakePillAction:update()
+	self.item:setJobDelta(self:getJobDelta());
+end
+
+function TakePillAction:start()
+	self.item:setJobType("Take a pill");
+	self.item:setJobDelta(0.0);
+end
+
+function TakePillAction:stop()
+    ISBaseTimedAction.stop(self);
+    self.item:setJobDelta(0.0);
+
+end
+
+function TakePillAction:perform()
+    self.item:getContainer():setDrawDirty(true);
+    self.item:setJobDelta(0.0);
+	--self.character:getBodyDamage():JustTookPill(self.item);
+    -- needed to remove from queue / start next.
+	--self.item:setCount(self.item:getCount() + 1);
+	print("and now we call the logic behind the pills..");
+	NicotinePatch.onUseItem(self.character,self.item);
+	ISBaseTimedAction.perform(self);
+end
+
+function TakePillAction:new (character, item, time)
+	local o = {}
+	setmetatable(o, self)
+	self.__index = self
+	o.character = character;
+	o.item = item;
+	o.stopOnWalk = false;
+	o.stopOnRun = false;
+	o.maxTime = time;
+	return o
+end
